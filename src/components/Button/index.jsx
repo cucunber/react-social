4fe8e5c53fabd72b92f7417ef406e74000e9fcc1
Andrey <mastercatpro.tm@gmail.com>
2022-02-03 01:31:59 +0300

    add node-sass and react-router-dom

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,19 @@
+import s from "./styles.module.scss";
 
-const Button = (props) => {
-    return(
-        <button>{props.text}</button>
-    )
-}
+const Button = ({text, theme="yellow", className="", disabled=false, textTransform="uppercase"}) => {
+
+  return (
+    <button
+      type="button"
+      disabled={disabled}
+      style={{
+          textTransform
+      }}
+      className={`${s.button} ${s[theme]} ${className}`}
+    >
+      {text}
+    </button>
+  );
+};
 
 export default Button;
